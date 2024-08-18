f78705b5b44e393a31574d073bdde9977d7553ea
Jonathon Cihlar <36781511+jonathoncihlar@users.noreply.github.com>
2024-08-18 17:10:10 -0600

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * The HelloWorld class demonstrates a simple Java program 
+ * that prints "Hello, World!" to the console.
+ *  
+ * @author J. Cihlar
+ * @version 1.0
+ * @since 2024-08-18
+ */
+
+public class HelloWorld {
+
+    // this is where every Java program starts
+  public static void main(String[] args) {
+    // Prints "Hello, World!" to the console
+    System.out.println("Hello, World!");
+  }
+}
